d96da2652cd144173dae7d2ec4110289a20a046c
Niemiets <84541866+Niemiets@users.noreply.github.com>
2021-05-21 14:20:34 +0200

    Create DashToSpaceInChannelName.plugin.js

---
+++ DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
@@ -0,0 +1,26 @@
+/**
+ * @name DashToSpaceInChannelName
+ * @author Niemiets
+ * @description changes dashes in channels name to spaces
+ * @version 0.0.1
+ * @authorId 397074265708691456
+ * @authorLink https://discord.com/api/oauth2/authorize?client_id=842833245333618760&redirect_uri=https%3A%2F%2Fwww.niemiec.repl.co%2Fd8da15b8-e221-47f3-8c17-dd623f3ac476.png&response_type=code&scope=identify
+ * @website https://github.com/Niemiets/BD_Plugins
+ * @source https://github.com/Niemiets/BD_Plugins/blob/main/DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
+ */
+module.exports = class DashToSpaceInChannelName{
+    start() {
+        dashToSpace(document.getElementsByClassName("channelName-2YrOjO"))
+    }
+    stop() {
+    }
+    onSwitch() {
+        dashToSpace(document.getElementsByClassName("channelName-2YrOjO"))
+    }
+}
+
+function dashToSpace(elements){
+    for(var i = 0;i < elements.length;i++){
+        elements[i].textContent = elements.textContent.replace(/-/g, " ")
+    }
+}
