1e1295cf13aaba9df021334a45adfc78b634589b
Niemiets <84541866+Niemiets@users.noreply.github.com>
2021-05-23 19:38:18 +0200

    Update AccountSwitcher.plugin.js

--- AccountSwitcher/AccountSwitcher.plugin.js
+++ AccountSwitcher/AccountSwitcher.plugin.js
@@ -9,27 +9,16 @@
  * @source https://github.com/Niemiets/BD_Plugins/blob/main/AccountSwitcher/AccountSwitcher.plugin.js
  * @updateUrl https://github.com/Niemiets/BD_Plugins/blob/main/AccountSwitcher/AccountSwitcher.plugin.js
  */
-console.log(this)
 module.exports = class AccountSwitcher {
-    load() {
-    }
     start() {
         var thisToken = BdApi.findModuleByProps("getToken").__proto__.getToken()
         var updated = false
         if(BdApi.loadData(PluginName, 0) !== undefined){
-            for(var i = 0;BdApi.loadData(PluginName, i) !== undefined;i++){
-                if(settings()[i].id == BdApi.findModuleByProps("getId").getId()){
-                    BdApi.saveData(PluginName, i, {"token": thisToken,"id": settings()[i].id})
-                    updated = true
-                }
-            }
-            if(updated == false){
-                BdApi.saveData(PluginName, settings().length, {"token": thisToken,"id": BdApi.findModuleByProps("getId").getId()})
-                console.log(BdApi.loadData(PluginName, settings().length))
-            }
-        }else{
-             BdApi.saveData(PluginName, 0, {"token": thisToken,"id": BdApi.findModuleByProps("getId").getId()})
-            console.log(BdApi.loadData(PluginName, 0))
+            updateToken()
+            updated = true
+        }
+        if(updated == false){
+            addToken(thisToken)
         }
     }
     stop() {
@@ -47,14 +36,26 @@ module.exports = class AccountSwitcher {
 
 function settings(){
     var data = []
-    for(var i = 0;BdApi.loadData(PluginName, i) !== undefined;i++){
+    for(var i = 0; BdApi.loadData(PluginName, i) !== undefined; i++){
         data.push(BdApi.loadData(PluginName, i))
     }
     return data
 }
 
+function updateToken(){
+    for(var i = 0; BdApi.loadData(PluginName, i) !== undefined; i++){
+        if(settings()[i].id == BdApi.findModuleByProps("getId").getId()){
+            BdApi.saveData(PluginName, i, {"token": thisToken, "id": settings()[i].id})
+        }
+    }
+}
+
+function addToken(token){
+    BdApi.saveData(PluginName, settings().length, {"token": token, "id": BdApi.findModuleByProps("getId").getId()})
+}
+
 function getIndexOfToken(){
-    for(var i = 0;i < settings().length;i++){
+    for(var i = 0; i < settings().length; i++){
         if(settings()[i].id == BdApi.findModuleByProps("getId").getId()){
             return i
         }
@@ -62,20 +63,20 @@ function getIndexOfToken(){
 }
 
 function changePosition(what,howMuch){
-    for(var i = 1;i < howMuch+1;i++){
-        switchPosition(what,what+i)
+    for(var i = 1; i < howMuch + 1; i++){
+        switchPosition(what, what + i)
     }
-    for(var i = -1;i > howMuch-1;i--){
-        switchPosition(what,what+i)
+    for(var i = -1; i > howMuch - 1; i--){
+        switchPosition(what, what + i)
     }
 }
 
-function switchPosition(what,withWhat){
-    data = BdApi.loadData(PluginName, what)
-    data2 = BdApi.loadData(PluginName, withWhat)
+function switchPosition(what, withWhat){
+    dataWhat = BdApi.loadData(PluginName, what)
+    dataWithWhat = BdApi.loadData(PluginName, withWhat)
     if(data !== undefined && data2 !== undefined){
-        BdApi.saveData(PluginName,withWhat,data)
-        BdApi.saveData(PluginName,what,data2)
+        BdApi.saveData(PluginName, withWhat, dataWhat)
+        BdApi.saveData(PluginName, what, dataWithWhat)
     }
 }
 
