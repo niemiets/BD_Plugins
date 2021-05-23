b60cf78723291155c34c849dc4714cc6e264529c
Niemiets <84541866+Niemiets@users.noreply.github.com>
2021-05-23 19:41:16 +0200

    Update DashToSpaceInChannelName.plugin.js

--- DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
+++ DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
@@ -7,7 +7,7 @@
  * @authorLink https://discord.com/api/oauth2/authorize?client_id=842833245333618760&redirect_uri=https%3A%2F%2Fwww.niemiec.repl.co%2Fd8da15b8-e221-47f3-8c17-dd623f3ac476.png&response_type=code&scope=identify
  * @website https://github.com/Niemiets/BD_Plugins
  * @source https://github.com/Niemiets/BD_Plugins/blob/main/DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
- * @updateUrl https://github.com/Niemiets/BD_Plugins/blob/main/DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/Niemiets/BD_Plugins/main/DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
  */
 module.exports = class DashToSpaceInChannelName{
     start() {
