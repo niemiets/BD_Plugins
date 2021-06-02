5ecd29cfbc391474ec0cfed47f25fd7a73cf7756
Niemiets <84541866+Niemiets@users.noreply.github.com>
2021-06-02 19:35:17 +0200

    Update DashToSpaceInChannelName.plugin.js

--- DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
+++ DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
@@ -1,28 +1,29 @@
 /**
  * @name DashToSpaceInChannelName
  * @author Niemiets
- * @description Changes dashes in channel names to spaces
- * @version 0.0.4
- * @authorId 397074265708691456
+ * @description Changes dashes in channels name to spaces
+ * @version 0.0.3
+ * @authorId 391014265108691456
  * @authorLink https://github.com/Niemiets
  * @website https://github.com/Niemiets/BD_Plugins
  * @source https://github.com/Niemiets/BD_Plugins/tree/main/DashToSpaceInChannelName
- * @updateUrl https://niemiets.github.io/BD_Plugins/DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/Niemiets/BD_Plugins/main/DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
  */
 module.exports = class DashToSpaceInChannelName{
-    start() {
-        this.dashToSpace(document.getElementsByClassName("channelName-2YrOjO"))
-        this.dashToSpace(document.getElementsByClassName("title-29uC1r"))
-    }
-    stop() {
-    }
-    onSwitch() {
-        this.dashToSpace(document.getElementsByClassName("channelName-2YrOjO"))
-        this.dashToSpace(document.getElementsByClassName("title-29uC1r"))
+    start() {}
+    stop() {}
+    observer(changes){
+        if(changes.addedNodes[0] != undefined){
+            if(changes.addedNodes[0].className.includes("container-3w7J-x")){
+                this.dashToSpace(document.getElementsByClassName("channelName-2YrOjO"))
+            }else if(changes.addedNodes[0].className.includes("title-3qD0b-")){
+                this.dashToSpace(document.getElementsByClassName("title-29uC1r"))
+            }
+        }
     }
     dashToSpace(elements){
         for(var i = 0;i < elements.length;i++){
-            elements[i].textContent = elements[i].textContent.replace(/-/g, " ")
+            BdApi.ReactDOM.render(elements[i].textContent.replace(/-/g, " "), elements[i])
         }
     }
 }
