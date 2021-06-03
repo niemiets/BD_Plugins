4733ea0595edcbabb03a9fca5a417d142c352ca2
Niemiets <84541866+Niemiets@users.noreply.github.com>
2021-06-03 12:11:58 +0200

    Update DashToSpaceInChannelName.plugin.js

--- DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
+++ DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
@@ -2,7 +2,7 @@
  * @name DashToSpaceInChannelName
  * @author Niemiets
  * @description Changes dashes in channels name to spaces
- * @version 0.0.3
+ * @version 0.0.4
  * @authorId 391014265108691456
  * @authorLink https://github.com/Niemiets
  * @website https://github.com/Niemiets/BD_Plugins
@@ -10,10 +10,13 @@
  * @updateUrl https://raw.githubusercontent.com/Niemiets/BD_Plugins/main/DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
  */
 module.exports = class DashToSpaceInChannelName{
-    start() {}
+    start() {
+        this.dashToSpace(document.getElementsByClassName("channelName-2YrOjO"))
+        this.dashToSpace(document.getElementsByClassName("title-29uC1r"))
+    }
     stop() {}
     observer(changes){
-        if(changes.addedNodes[0] != undefined){
+        if(changes.addedNodes[0] != undefined && typeof changes.addedNodes[0].className == "string"){
             if(changes.addedNodes[0].className.includes("container-3w7J-x")){
                 this.dashToSpace(document.getElementsByClassName("channelName-2YrOjO"))
             }else if(changes.addedNodes[0].className.includes("title-3qD0b-")){
