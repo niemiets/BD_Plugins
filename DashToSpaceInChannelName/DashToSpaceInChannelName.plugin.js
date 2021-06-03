f9dc0eb2f59fcd4e7a5506f1ebb2c20101e13ab3
Niemiets <84541866+Niemiets@users.noreply.github.com>
2021-06-03 14:57:49 +0200

    Update DashToSpaceInChannelName.plugin.js

--- DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
+++ DashToSpaceInChannelName/DashToSpaceInChannelName.plugin.js
@@ -2,7 +2,7 @@
  * @name DashToSpaceInChannelName
  * @author Niemiets
  * @description Changes dashes in channels name to spaces
- * @version 0.0.4
+ * @version 0.0.5
  * @authorId 391014265108691456
  * @authorLink https://github.com/Niemiets
  * @website https://github.com/Niemiets/BD_Plugins
@@ -14,7 +14,12 @@ module.exports = class DashToSpaceInChannelName{
         this.dashToSpace(document.getElementsByClassName("channelName-2YrOjO"))
         this.dashToSpace(document.getElementsByClassName("title-29uC1r"))
     }
-    stop() {}
+    stop() {
+        for(var i = 0;document.getElementsByClassName("channelName-2YrOjO").length > i;i++){
+            BdApi.ReactDOM.render(BdApi.findModuleByProps("getChannel").getChannel(document.getElementsByClassName("mainContent-u_9PKf")[i].dataset.listItemId.slice(11,document.getElementsByClassName("mainContent-u_9PKf")[i].dataset.listItemId.length)).name, document.getElementsByClassName("channelName-2YrOjO")[i])
+        }
+        BdApi.ReactDOM.render(BdApi.findModuleByProps("getChannel").getChannel(BdApi.findModuleByProps("getChannelId").getChannelId()).name,document.getElementsByClassName("title-29uC1r")[0])
+    }
     observer(changes){
         if(changes.addedNodes[0] != undefined && typeof changes.addedNodes[0].className == "string"){
             if(changes.addedNodes[0].className.includes("container-3w7J-x")){
